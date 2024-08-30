94c41bd2a9fa25298ed1ce3ab71954f7e2f2d1b5
korey-byte <korey.sansone@yahoo.com>
2024-08-30 16:42:11 -0500

    Update utils_koreys.py - Iteration 1 - custom

--- utils_korey.py
+++ utils_korey.py
@@ -1,6 +1,8 @@
+
+
 ''' ITERATION 1
 
-Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
+Module: 2K Sales LLC - Reasonably priced shoes by a resonable seller
 
 This module provides a simple, reusable foundation for my analytics projects. 
 When we work hard to write useful code, we want it to be reusable.
@@ -28,7 +30,7 @@ I'll test it in an online interpreter to ensure this version runs correctly befo
 # Declare a global variable named byline.
 #####################################
 
-byline: str = 'Stellar Analytics: Delivering Professional Insights'
+byline: str = '2K Sales LLC - Reasonably priced shoes by a resonable seller'
 
 #####################################
 # Define a main() function for this module.
@@ -53,3 +55,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
