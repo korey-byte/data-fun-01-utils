33da76fe6315f08179853eda47699d79f4bf8351
korey-byte <korey.sansone@yahoo.com>
2024-08-30 22:15:05 -0500

    Iteration 4 - add stats

--- utils_korey.py
+++ utils_korey.py
@@ -1,16 +1,18 @@
-''' ITERATION 3
-
-Module: 2K Sales LLC - Reasonbly priced shoes by a reasonable seller
-
-This module provides a simple, reusable foundation for my analytics projects.
-
 Process:
-In this third iteration, I declare additional variables to show skills with different data types.
+In this fourth iteration, I introduce some basic statistics using Python.
+    - min() is a built in function to find the smallest value passed in
+    - max() is a built in function to find the largest value passed in
+    - The statistics module offers methods to calculate mean and standard deviation.
 '''
 
 ###########################################
-# Declare global variables - keep byline at the end
-# We will use this information in a smarter byline
+# Import Modules at the Top
+###########################################
+
+import statistics
+
+###########################################
+# Declare global variables
 ###########################################
 
 # Boolean variable to indicate if the company is the best company ever
@@ -19,11 +21,24 @@ is_best_company_ever: bool = True
 # Integer variable for the number of years in biness
 years_in_biness: int = 5
 
+* Float variable for the average koreys pretty good homework scores
+koreys_average_scores: float = 4.0
+
 # List of strings representing the softwares i'm kind of sort of good at
 skills_semi_mastered: list = ["Salesforce", "Excel", "Power_bi"]
 
 # List of floats representing my latest and greatest homework scores
 Koreys_pretty_good_homework_scores: list = [8.0, 3.0, 2.0, 8.0, 4.0]
+
+###########################################
+# Calculate Basic Statistics
+###########################################
+
+# Calculate basic stats using built-in functions min(), max() and statistics module functions mean() and stdev().
+min_score: float = min(koreys_pretty_good_homework_scores)
+max_score: float = max(koreys_pretty_good_homework_scores)
+mean_score: float = statistics mean (koreys_pretty_good_homework_scores)
+stdev_score: float = statistics.stdev(koreys_pretty_good_homework_scores)
 
 ###########################################
 # Declare a global variable named byline.
@@ -55,3 +70,4 @@ def get_byline() -> str:
 def main() -> None:
     '''Print results of get_byline() when main() is called.'''
     print(get_byline())
+
